276d33c6769161916d6d973b3d2312766c455d66
AlianzaFamiliar <ti.soporte@alianzafamiliar.mx>
2025-02-13 16:53:17 -0600

    Añadiendo estructura al index

---
+++ navbar.js
@@ -0,0 +1,31 @@
+
+const navbar = document.getElementsByClassName("header main");
+const itemsnavbar = [
+
+    {
+        nombre: "inicio",
+        link: "",
+    },
+    {
+        nombre: "Créditos Fovissste",
+        link: "",
+    },
+    {
+        nombre: "Créditos Infonavit",
+        link: "",
+    },
+    {
+        nombre: "Créditos Bancarios",
+        link: "",
+    },
+    {
+        nombre: "Servicios Adicionales",
+        link: "",
+    },
+    {
+        nombre: "Sucursales",
+        link: "",
+    }
+]
+
+    
